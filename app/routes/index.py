117622ea3f4560b7871b83e109421431d397a793
Julian Mazzitelli <mazzitell.julian@gmail.com>
2015-01-31 18:38:37 -0500

    sdsdsd

--- app/routes/index.py
+++ app/routes/index.py
@@ -3,3 +3,8 @@ from app import app
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
+
+
+@app.route('/test')
+def main():
+	return "hello world"
--- app/static/js/home/home-controller.js
+++ app/static/js/home/home-controller.js
@@ -1,3 +1,11 @@
 angular.module('gotsylla')
-  .controller('HomeController', ['$scope', function ($scope) {
-  }]);
+	.controller('HomeController', ['$scope', function ($scope) {
+ 		$scope.word = "car";
+
+ 		$scope.words;
+
+ 		$.get("http://127.0.0.1:5000/test", function(data){
+ 			console.log(data);
+ 			$scope.words = data;
+ 		})
+  	}]);
